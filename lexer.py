1d3059be7da21324c653e6f9f3b2084a8f2aad97
UNGARO Cosimo <cosimo.ungaro@telecomnancy.eu>
2023-10-13 13:54:03 +0200

    Ajout des opérateurs

--- lexer.py
+++ lexer.py
@@ -1,15 +1,28 @@
-file=open('input', 'r')
-data=[element.replace('\n','') for element in file.readlines()]
+data=open('input', 'r').readlines()
 
 operators=['if', 'else', 'elif', 'while']
+symbols = ['{', '}', '(', ')', '[', ']', '.', '"', '*', '\n', ':', ',']
+other_symbols = ['\\', '/*', '*/']
+keywords = ['public', 'class', 'void', 'main', 'String', 'int']
+KEYWORDS = symbols + other_symbols + keywords
+
 
 def main():
     lexemes=[]
 
     for line in data:
-        for character in line:
-            if line.index(character)<len(line):
-                print(character)
+        lexeme=''
+        for (i,character) in enumerate(line):
+            if i<len(line)-1:
+                if character!=' ':
+                    lexeme+=character
+                if line[i+1]==' ' or line[i+1]=='\n':
+                    lexemes.append(lexeme)
+                    lexeme=''
+
+    lexemes=[element for element in lexemes if element!='']
+
+    print(lexemes)
 
 if __name__=='__main__':
     main()
